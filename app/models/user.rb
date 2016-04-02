192a2c9d2ff4b1daa1f82b897e594580366154c2
William Butler <wbutler@terpmail.umd.edu>
2016-04-01 22:11:34 -0400

    Updated the UI of the Website - Completed Sign In, Sign Up, Sign Out, and Forgot Password - Some EMAIL aspects should work too (such as the Change Password page - Generated when you forget your password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         # Specifies a VALID password.
          validates :password, :presence => true,
                      :on => :create,
-                     :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\`\~\!\@\#\$\%\^\&\*\(\)\-\_\=\+\[\{\]\}\;\'\"\,\<\.\>\/\?\|\\]).*\Z/ }
+                     :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*\Z/ }
 end
